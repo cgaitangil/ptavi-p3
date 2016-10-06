105265e31d25b567ca6f9c08bfb40811802d586f
cgaitangil <cgaitangil@gmail.com>
2016-10-06 14:10:31 +0200

    Lista ok sin gettags

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -8,45 +8,48 @@ class SmallSMILHandler(ContentHandler):
     '''Clase para extraer la información relevante del fichero XML'''
     
     def __init__ (self):
-        self.rlayout = ''
-        self.region = ''
-        self.img = ''
-        self.audio = ''
-        self.textstream = ''
+        
+#Para iterar        self.etiquetas = ['root-layout', 'region', 'img', 'audio', 
+#                          'textsstream']
+        
         self.atributos_rlayout = ['width', 'height', 'background-color']
         self.atributos_region = ['id', 'top', 'bottom', 'left', 'right']
         self.atributos_img = ['src', 'region', 'begin', 'dur']
         self.atributos_audio = ['src', 'begin', 'dur']
         self.atributos_textstream = ['src', 'region']
         
-    def get_tags(self, name, attrs):
-        '''Método que devuelve lista con las etiquetas'''
+        self.dicc_rlayout = {}
+        self.dicc_region = {}
+        self.dicc_img = {}
+        self.dicc_audio = {}
+        self.dicc_textstream = {}
         
-             
-   
-    
+        self.lista_etiqs = [self.dicc_rlayout, self.dicc_region, self.dicc_img,
+                            self.dicc_audio, self.dicc_textstream]
+                            
+        
     def startElement(self, name, attrs):
         '''Método empleado al abrir una etiqueta'''
+        
         if name == 'root-layout':
             for atributo in self.atributos_rlayout:
-                self.rlayout = attrs.get(atributo, '')
-                print(str(atributo) + ' ' + self.rlayout)
-        
+                self.dicc_rlayout[atributo] = attrs.get(atributo, '')
         if name == 'region':
             for atributo in self.atributos_region:
-                self.region = attrs.get(atributo, '')
-                print(str(atributo) + '-->' + self.region)
-        
-    def contenido(self, name, attrs):
-        '''Método empleado para extraer la información relevante'''
-        
-        
+                self.dicc_region[atributo] = attrs.get(atributo, '')
+        if name == 'img':
+            for atributo in self.atributos_img:
+                self.dicc_img[atributo]=attrs.get(atributo,'')
+        if name == 'audio':
+            for atributo in self.atributos_audio:
+                self.dicc_audio[atributo] = attrs.get(atributo, '')
+        if name == 'textstream':
+            for atributo in self.atributos_textstream:
+                self.dicc_textstream[atributo] = attrs.get(atributo, '')
+                
 if __name__ == '__main__':
 
     parser = make_parser()
     shandler = SmallSMILHandler()
     parser.setContentHandler(shandler)
     parser.parse(open('karaoke.smil'))
-    
-    
-    
