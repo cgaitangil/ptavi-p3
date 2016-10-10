5807ccde52a15d4208c34a22374d4da00d3d946e
Carlos Gaitan Gil <cgaitan@gamma18.aulas.gsyc.urjc.es>
2016-10-10 10:31:38 +0200

    To json done

--- karaoke.py
+++ karaoke.py
@@ -5,35 +5,47 @@ import sys
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
+import json
+
+def to_json(nombre, fich):
+    f_json = nombre[:-4] + "json"
+    json.dump(fich, open(f_json  , 'w'))
+    return f_json
 
 if __name__ == '__main__':
 
     parser = make_parser()
     fhandler = SmallSMILHandler()
     parser.setContentHandler(fhandler)
+
+    
     try:
-        parser.parse(open(sys.argv[1]))
+        fichero = sys.argv[1]
     except IndexError:
-        sys.exit('Expected File')
+        sys.exit('Usage: python3 karaoke.py file.smil.')
+
+    parser.parse(open(fichero))
         
     etiqs = fhandler.lista_etiqs # Guardamos la lista para trabajar con ella
     
-    print(etiqs)
-    print('')
-    
     for line in etiqs:
         exit = line['etiqueta']
-        print(line)        
+        #print(line)        
         for attr in line:
             if attr == 'etiqueta':
                 exit = exit
             else:
                 if not line[attr] == '':
-                    exit = exit + ',' + attr + '="' + line[attr] + '"'
-        exit = exit + '\n'
+                    exit = exit + '\t' + attr + '="' + line[attr] + '"'
+       # exit = exit + '\n'
         print(exit)
+
+    to_json(fichero, etiqs)
+    
+    
+    
+    
         
-        print('')
     
     
     
