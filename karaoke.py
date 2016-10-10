c788b503790199161957442fc1c9b18c6aed1f37
Carlos Gaitan Gil <cgaitan@gamma18.aulas.gsyc.urjc.es>
2016-10-10 12:09:34 +0200

    pep8 pasado

--- karaoke.py
+++ karaoke.py
@@ -8,59 +8,53 @@ from smallsmilhandler import SmallSMILHandler
 import json
 import urllib
 
-def to_json(nombre, fich):
-    f_json = nombre[:-4] + "json"
-    json.dump(fich, open(f_json  , 'w'))
-    return f_json
+
+class KaraokeLocal(SmallSMILHandler):
+
+    def __init__(self, fichero):
+        parser = make_parser()
+        fhandler = SmallSMILHandler()
+        parser.setContentHandler(fhandler)
+        parser.parse(open(fichero))
+        self.etiqs = fhandler.get_tags()
+
+    def __str__(self):
+        for line in self.etiqs:
+            exit = line['etiqueta']
+            for attr in line:
+                if attr == 'etiqueta':
+                    exit = exit
+                else:
+                    if not line[attr] == '':
+                        exit = exit + '\t' + attr + '="' + line[attr] + '"'
+            print(exit)
+
+    def to_json(self, nombre):
+        f_json = nombre[:-4] + "json"
+        json.dump(self.etiqs, open(f_json, 'w'))
+        return f_json
+
+    def do_local(self):
+        for line in self.etiqs:
+            for attr in line:
+                if attr == 'src' and line[attr][:4] == 'http':
+                    local = line[attr].split('/')[-1]
+                    urllib.request.urlretrieve(line[attr], local)
+                    line[attr] = local
 
 if __name__ == '__main__':
 
-    parser = make_parser()
-    fhandler = SmallSMILHandler()
-    parser.setContentHandler(fhandler)
-
-    
     try:
         fichero = sys.argv[1]
     except IndexError:
         sys.exit('Usage: python3 karaoke.py file.smil.')
 
-    parser.parse(open(fichero))
-        
-    etiqs = fhandler.lista_etiqs # Guardamos la lista para trabajar con ella
-    
-    for line in etiqs:
-        exit = line['etiqueta']
-        #print(line)        
-        for attr in line:
-            if attr == 'etiqueta':
-                exit = exit
-            else:
-                if not line[attr] == '':
-                    exit = exit + '\t' + attr + '="' + line[attr] + '"'
-       # exit = exit + '\n'
-        print(exit)
+    objeto = KaraokeLocal(fichero)
 
-    to_json(fichero, etiqs)
+    objeto.__str__()
 
-    print('---------------')
-    print('')
+    objeto.to_json(fichero)
 
-    for line in etiqs:
-        print(line)
-        for attr in line:
-                   
-            if attr == 'src' and line[attr][:4] == 'http':
-                local = line[attr].split('/')[-1]
-                print(local)
-                urllib.request.urlretrieve(line[attr], local)
-                line[attr] = local
-    
-    
-    
-    
-        
-    
-    
-    
-        
+    objeto.do_local()
+
+    objeto.__str__()
